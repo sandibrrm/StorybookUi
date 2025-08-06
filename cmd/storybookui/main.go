94eeed1bd32e2af9b62aaaa87e54d3eb430fc868
Sandi <43517564+sandibrrm@users.noreply.github.com>
2025-08-07 00:31:12 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/storybookui/main.go
@@ -0,0 +1,23 @@
+// cmd/storybookui/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "storybookui/internal/storybookui"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := storybookui.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
